f615a2e407c7052b5dd7bbcbc124bd5db179f06c
SwoopX <rol_swoop@gmx.de>
2013-08-19 16:29:24 +0200

    Code cleanup

--- code/logs.php
+++ code/logs.php
@@ -1,15 +1,12 @@
 <?php
-	//if ($_SERVER["REQUEST_METHOD"]=="POST") {
-		//if (getPostVar("process", "") == "save") {
-			//	setcookie("LogFilePath", getPostVar("logfile", ""), "/");
-				//setcookie("language",$language,mktime(9,9,9,9,9,9999));
-		//}
-	//}
-	
-	
-	if (isset($_POST["logfile"])) {
-		$logfile = $_POST["logfile"];
-		$_COOKIE["logfile"] = $logfile;
-		setcookie("logfile",$logfile,mktime(9,9,9,9,9,9999),"/");
+	if ($_SERVER["REQUEST_METHOD"]=="POST") {
+		/*****************************************************************/
+		/*****************************************************************/
+		if (isset($_POST["logfile"])) {
+			$logfile = $_POST["logfile"];
+			$_COOKIE["logfile"] = $logfile;
+			setcookie("logfile",$logfile,mktime(9,9,9,9,9,9999),"/");	
+			return true;
+		}
 	}
 ?>
