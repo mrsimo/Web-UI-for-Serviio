7413f18ec377b013cbead4c4dc52b73ac7a49645
SwoopX <rol_swoop@gmx.de>
2013-08-25 15:45:41 +0200

    Updated logfile cookie lifetime

--- code/logs.php
+++ code/logs.php
@@ -5,7 +5,7 @@
 		if (isset($_POST["logfile"])) {
 			$logfile = $_POST["logfile"];
 			$_COOKIE["logfile"] = $logfile;
-			setcookie("logfile",$logfile,mktime(9,9,9,9,9,9999),"/");	
+			setcookie("logfile",$logfile,mktime(12,0,0,9,9,2020),"/");	
 			return true;
 		}
 	}
