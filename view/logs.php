fe4296be3498383be5469dc35b65b6e74d6577bd
SwoopX <rol_swoop@gmx.de>
2013-08-25 15:54:15 +0200

    Minor layout and text corrections

--- view/logs.php
+++ view/logs.php
@@ -6,11 +6,10 @@
 <ul id="logsFileTab" class="shadetabs">
 	<li><a href="#" rel="logs1" class="selected"><?php echo tr('tab_log_file','Serviio log file')?></a></li>
 </ul>
-<br>
 
 <div style="border:1px solid gray; width:98%; margin-bottom: 1em; padding: 10px">
 	<div id="logs1" class="tabcontent">
-		<?php echo tr('tab_logs_file_description','The below shows the content of the Serviio generated log file.')?>
+		<?php echo tr('tab_logs_file_description','The below shows the content of the selected Serviio generated log file.')?>
 		<br>
 		<br>
 		<table>
@@ -52,35 +51,34 @@
 
 <div style="border:1px solid gray; width:98%; margin-bottom: 1em; padding: 10px">
 	<div id="logs2" class="tabcontent">
-		<br>
-			<?php
-				if (!empty($_COOKIE["logfile"])) {
-				$log = $_COOKIE["logfile"];
-				$file = fopen( $log, "r") or exit('<strong><span style="color:#FF0000;text-align:left;">'.tr('tab_log_open_error','Unable to open Serviio log file!').'</span></strong>');
-				$stack = array();
-				//Output a line of the file until the end is reached
-				while(!feof($file))
-				{
-					array_push($stack, fgets($file));
+		<?php
+			if (!empty($_COOKIE["logfile"])) {
+			$log = $_COOKIE["logfile"];
+			$file = fopen( $log, "r") or exit('<strong><span style="color:#FF0000;text-align:left;">'.tr('tab_log_open_error','Unable to open Serviio log file!').'</span></strong>');
+			$stack = array();
+			//Output a line of the file until the end is reached
+			while(!feof($file))
+			{
+				array_push($stack, fgets($file));
+			}
+			fclose($file);
+			$reversed = array_reverse($stack);
+			foreach($reversed as $value) {
+				if (strpos($value,'WARN') !== false) {
+					echo "<span style='background-color:yellow'>".$value. "</span><br>";
 				}
-				fclose($file);
-				$reversed = array_reverse($stack);
-				foreach($reversed as $value) {
-					if (strpos($value,'WARN') !== false) {
-						echo "<span style='background-color:yellow'>".$value. "</span><br>";
-					}
-					elseif (strpos($value,'ERROR') !== false) {
-						echo "<span style='background-color:red'>".$value. "</span><br>";
-					}
-					else {
-						echo $value. "<br>";
-					}
-				}
+				elseif (strpos($value,'ERROR') !== false) {
+					echo "<span style='background-color:red'>".$value. "</span><br>";
 				}
 				else {
-					echo tr('tab_log_empty','No log file selected.');
+					echo $value. "<br>";
 				}
-			?>
+			}
+			}
+			else {
+				echo tr('tab_log_empty','No log file selected.');
+			}
+		?>
 		<br>
 	</div>
 </div>
