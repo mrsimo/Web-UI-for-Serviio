e1a72f4bae7ca742c2401372f556b39f732410f8
SwoopX <rol_swoop@gmx.de>
2013-11-17 00:58:03 +0100

    Updated data

--- view/about.php
+++ view/about.php
@@ -21,8 +21,8 @@
 ?>
 <br>
 <br>
-<?php echo tr('tab_about_text','DLNA media streaming server<br>Copyright 2009-2012 Petr Nejedly<br>
-Web UI for Serviio maintained by Mark Pemberton<br>
+<?php echo tr('tab_about_text','DLNA media streaming server<br>Copyright 2009-2013 Petr Nejedly<br>
+Web UI for Serviio maintained by Sascha Eilers<br>
 <a href="http://serviio.org">http://serviio.org</a><br><br>
 This product may use movie metadata provided bt <a href="http://www.themoviedb.org/">TheMovieDb.org</a>, please consider contributing to the database.<br>
 This product may use TV metadata provided by <a href="http://www.thetvdb.com/">TheTVDb.com</a>, please consider contributing to the database.<br>
