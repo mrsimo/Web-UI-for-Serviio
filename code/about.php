ef3a07f4a27245ea05c459bf1cf18f7e678a5ba5
SwoopX <rol_swoop@gmx.de>
2013-12-18 17:37:14 +0100

    Implemented code from license_ul.php and changed response if license file has no .lic extension

--- code/about.php
+++ code/about.php
@@ -1,3 +1,30 @@
 <?php
-/* placeholder */
+    if ($_SERVER["REQUEST_METHOD"]=="POST") {
+        include("../config.php");
+        include("../lib/RestRequest.inc.php");
+        include("../lib/serviio.php");
+
+        // initiate call to service
+        $serviio = new ServiioService($serviio_host,$serviio_port);
+
+        /*****************************************************************/
+        /*****************************************************************/
+        if (getPostVar("process", "") == "upload") {
+            
+            $data = getPostVar("licenseData", "");
+            $filename = getPostVar("filename", "");
+            
+            if (substr($filename,-4) != ".lic") {
+                header("Content-type: text/plain");
+                echo '<?xml version="1.0" encoding="UTF-8" ?>';
+                echo '<result>';
+                echo '<errorCode>560</errorCode>';
+                echo '</result>';
+                exit;
+            }
+            
+            $errorCode = $serviio->putLicenseUpload($data);
+            return $errorCode;
+        }
+    }
 ?>
