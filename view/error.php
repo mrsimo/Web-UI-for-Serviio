e80413f7142c4bd58aebbcec341f3b5814a9a022
SwoopX <rol_swoop@gmx.de>
2014-01-06 11:55:49 +0100

    Obsolete

--- view/error.php
+++
@@ -1,7 +0,0 @@
-<div align="center">
-<img src="images/error_button.gif" alt="Error">
-<br>
-<?php echo $serviio->error?>
-<br>
-<?php echo tr('error_1','Cannot connect to Serviio server, it does not seem to be running.')?>
-</div>
