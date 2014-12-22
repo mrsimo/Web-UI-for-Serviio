a85a69b3fbbb661226a1b780e52a9cbd001a6899
SwoopX <rol_swoop@gmx.de>
2014-12-22 11:16:07 +0100

    Updated required version and UI version number

--- config.php
+++ config.php
@@ -1,11 +1,11 @@
 <?php
-    $webUIver = "1.5.5";
+    $webUIver = "1.5.6";
     $debugLevel = "none"; // none, debug
     $debugLoc = "none"; // none, screen
     $serviio_host = "127.0.0.1";
     $serviio_port = "23423";
     $serviidb_url = "http://www.serviidb.com/api/";
-    $version_req = "1.3";
+    $version_req = "1.5";
 
     # set appropriate encoding
     mb_internal_encoding("UTF-8");
